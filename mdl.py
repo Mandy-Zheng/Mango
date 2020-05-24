67d80efff40a444d0c70795541a38a0c02a51af6
qtgeo1248 <gzhou7819@gmail.com>
2020-05-24 15:58:55 -0400

    did it?

--- mdl.py
+++ mdl.py
@@ -301,7 +301,7 @@ def p_command_basename(p):
     commands.append(cmd)
 
 def p_command_vary(p):
-    """command : VARY SYMBOL NUMBER NUMBER NUMBER NUMBER"""
+    """command : VARY SYMBOL NUMBER NUMBER NUMBER NUMBER VARY_TYPE"""
     cmd = {'op' : p[1], 'args' : p[3:], 'knob' : p[2]}
     symbols[p[2]] = ['knob', 0]
     commands.append(cmd)
