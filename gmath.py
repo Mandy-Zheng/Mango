71c8da521a6df8741828a646902aefca44303ffc
qtgeo1248 <gzhou7819@gmail.com>
2020-05-25 19:52:24 -0400

    housecleaned gmath

--- gmath.py
+++ gmath.py
@@ -1,17 +1,11 @@
 import math
 from display import *
 
-
-  # IMPORANT NOTE
-
   # Ambient light is represeneted by a color value
-
   # Point light sources are 2D arrays of doubles.
   #      - The fist index (LOCATION) represents the vector to the light.
   #      - The second index (COLOR) represents the color.
-
-  # Reflection constants (ka, kd, ks) are represened as arrays of
-  # doubles (red, green, blue)
+  # Reflection constants (ka, kd, ks) are represened as arrays of doubles (red, green, blue)
 
 AMBIENT = 0
 DIFFUSE = 1
@@ -20,9 +14,7 @@ LOCATION = 0
 COLOR = 1
 SPECULAR_EXP = 4
 
-#lighting functions
-def get_lighting(normal, view, ambient, light, symbols, reflect ):
-
+def get_lighting(normal, view, ambient, light, symbols, reflect):
     n = normal[:]
     normalize(n)
     normalize(light[LOCATION])
@@ -38,7 +30,6 @@ def get_lighting(normal, view, ambient, light, symbols, reflect ):
     i[GREEN] = int(a[GREEN] + d[GREEN] + s[GREEN])
     i[BLUE] = int(a[BLUE] + d[BLUE] + s[BLUE])
     limit_color(i)
-
     return i
 
 def calculate_ambient(alight, reflect):
@@ -50,9 +41,7 @@ def calculate_ambient(alight, reflect):
 
 def calculate_diffuse(light, reflect, normal):
     d = [0, 0, 0]
-
-    dot = dot_product( light[LOCATION], normal)
-
+    dot = dot_product(light[LOCATION], normal)
     dot = dot if dot > 0 else 0
     d[RED] = light[COLOR][RED] * reflect['red'][DIFFUSE] * dot
     d[GREEN] = light[COLOR][GREEN] * reflect['green'][DIFFUSE] * dot
@@ -62,11 +51,11 @@ def calculate_diffuse(light, reflect, normal):
 def calculate_specular(light, reflect, view, normal):
     s = [0, 0, 0]
     n = [0, 0, 0]
-
     result = 2 * dot_product(light[LOCATION], normal)
-    n[0] = (normal[0] * result) - light[LOCATION][0]
-    n[1] = (normal[1] * result) - light[LOCATION][1]
-    n[2] = (normal[2] * result) - light[LOCATION][2]
+
+    n[0] = normal[0] * result - light[LOCATION][0]
+    n[1] = normal[1] * result - light[LOCATION][1]
+    n[2] = normal[2] * result - light[LOCATION][2]
 
     result = dot_product(n, view)
     result = result if result > 0 else 0
@@ -82,37 +71,22 @@ def limit_color(color):
     color[GREEN] = 255 if color[GREEN] > 255 else color[GREEN]
     color[BLUE] = 255 if color[BLUE] > 255 else color[BLUE]
 
-#vector functions
-#normalize vetor, should modify the parameter
-def normalize(vector):
-    magnitude = math.sqrt( vector[0] * vector[0] +
-                           vector[1] * vector[1] +
-                           vector[2] * vector[2])
+
+def normalize(vector): #should modify the parameter
+    magnitude = math.sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2])
     for i in range(3):
         vector[i] = vector[i] / magnitude
 
-#Return the dot porduct of a . b
 def dot_product(a, b):
     return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
 
-#Calculate the surface normal for the triangle whose first
-#point is located at index i in polygons
-def calculate_normal(polygons, i):
-
+def calculate_normal(polygons, i): #Calculate surface normal for triangle whose first point is index i
     A = [0, 0, 0]
     B = [0, 0, 0]
     N = [0, 0, 0]
-
-    A[0] = polygons[i+1][0] - polygons[i][0]
-    A[1] = polygons[i+1][1] - polygons[i][1]
-    A[2] = polygons[i+1][2] - polygons[i][2]
-
-    B[0] = polygons[i+2][0] - polygons[i][0]
-    B[1] = polygons[i+2][1] - polygons[i][1]
-    B[2] = polygons[i+2][2] - polygons[i][2]
-
-    N[0] = A[1] * B[2] - A[2] * B[1]
-    N[1] = A[2] * B[0] - A[0] * B[2]
-    N[2] = A[0] * B[1] - A[1] * B[0]
-
+    for j in range(0, 3):
+        A[j] = polygons[i + 1][j] - polygons[i][j]
+        B[j] = polygons[i + 2][j] - polygons[i][j]
+    for j in range(0, 3):
+        N[j] = A[(j + 1) % 3] * B[(j + 2) % 3] - A[(j + 2) % 3] * B[(j + 1) % 3]
     return N
