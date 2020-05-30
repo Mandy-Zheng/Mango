95f7a5753c5ba4d97706d6ff8c8fbac5ddf452e0
qtgeo1248 <gzhou7819@gmail.com>
2020-05-30 16:41:06 -0400

    minor fix

--- script.py
+++ script.py
@@ -81,7 +81,7 @@ def second_pass(commands, num_frames):
                         p = (frac + t - lower) / (upper - lower)
                         a = -2.0 * (value2 - value1) / (0.5) ** 2
                         vi = a / -2.0
-                        frames[i][knob] = 1 - (0.5 * a * p ** 2 + vi * p) * r ** num
+                        frames[i][knob] = 1 - (0.5 * a * p ** 2 + vi * p) * r ** (2 * num)
                         print(str(frames[i][knob]))
     return frames
 
